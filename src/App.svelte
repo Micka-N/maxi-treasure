82424418ce050d611e3e3cbe392913439274a868
Micka N <m.nouhaud@icloud.com>
2025-08-25 12:48:54 +0200

    Page d'accueil OK

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,9 @@
 <script>
 
     // Imports:----------------------------------------------------------------
+    import Button from "./Button.svelte";
     // Variables:--------------------------------------------------------------
+    let currentPage = $state("accueil");
     // Fonctions:--------------------------------------------------------------
     // Au démarrage de l'application: -----------------------------------------
 
@@ -13,7 +15,13 @@
 
 <!-- 🔧 HTML  ═══════════════════════════ ⬇︎ ⬇︎ ⬇︎ ═════════════════════════════ 🔧 HTML -->
 <main>
-
+    {#if currentPage === "accueil"}
+        <section id="accueil">
+            <h1>MAXI-TREASURE</h1>
+            <Button textButton="Créer joueur" />
+            <Button textButton="Choisir joueur" />
+        </section>
+    {/if}
 </main>
 
 
@@ -21,5 +29,39 @@
 
 <!-- 🎨 CSS  ═════════════════════════════ ⬇︎ ⬇︎ ⬇︎ ═════════════════════════════ 🎨 CSS -->
 <style>
+
+    main {
+        border: 3px solid red;
+        width: 100%;
+        height: 100%;
+    }
+
+    section {
+        border: 3px solid blue;
+        width: 100%;
+        height: 100%;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        gap: 30px;
+    }
+
+    section h1 {
+        font-size: 3.5rem;
+        color: var(--secondary-color);
+    }
+
+    /* Page d'accueil ----------------------------*/
+    #accueil {
+        background-color: #000000;
+        background-image: url(../public/treasure.jpg);
+        background-position: bottom;
+        background-repeat: no-repeat;
+        background-size: 60%;
+    }
+
+
 
 </style>
---
+++ src/Button.svelte
@@ -0,0 +1,49 @@
+<script>
+    export let textButton = "Bouton";
+</script>
+
+
+
+
+<!-- 🔧 HTML  ═══════════════════════════ ⬇︎ ⬇︎ ⬇︎ ═════════════════════════════ 🔧 HTML -->
+<button class="button-32">{textButton}</button>
+
+
+
+
+<!-- 🎨 CSS  ═════════════════════════════ ⬇︎ ⬇︎ ⬇︎ ═════════════════════════════ 🎨 CSS -->
+
+<style>
+    
+.button-32 {
+    background-color: var(--primary-color);
+    border-radius: 12px;
+    color: var(--fourth-color);
+    cursor: pointer;
+    font-weight: bold;
+    padding: 10px 15px;
+    text-align: center;
+    transition: 200ms;
+    width: max-content;
+    box-sizing: border-box;
+    border: 0;
+    font-size: 16px;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+}
+
+.button-32:not(:disabled):hover,
+.button-32:not(:disabled):focus {
+    outline: 0;
+    background: var(--primary-color);
+    box-shadow: 0 0 0 2px var(--secondary-color), 0 3px 8px 0 var(--secondary-color);
+}
+
+.button-32:disabled {
+    filter: saturate(0.2) opacity(0.5);
+    -webkit-filter: saturate(0.2) opacity(0.5);
+    cursor: not-allowed;
+}
+
+</style>
